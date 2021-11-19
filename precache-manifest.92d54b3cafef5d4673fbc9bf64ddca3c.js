f1f9fac3b36398ed40d24b3dfc043dd9813d7ac8
zhouhuan <754865975@qq.com>
2021-11-19 14:39:50 +0800

    Updates

---
+++ precache-manifest.92d54b3cafef5d4673fbc9bf64ddca3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21c9a57dbbb5aad46b20",
+    "url": "./static/css/main.9cc6fdb7.chunk.css"
+  },
+  {
+    "revision": "21c9a57dbbb5aad46b20",
+    "url": "./static/js/main.21c9a57d.chunk.js"
+  },
+  {
+    "revision": "2bc52cc3505175b59d31",
+    "url": "./static/css/1.575b2b56.chunk.css"
+  },
+  {
+    "revision": "2bc52cc3505175b59d31",
+    "url": "./static/js/1.2bc52cc3.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "340ba7081bc1b09c5581a2a424f8c550",
+    "url": "./index.html"
+  }
+];
