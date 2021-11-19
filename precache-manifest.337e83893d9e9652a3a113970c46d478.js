a295fcf9552330b21b7a03ab39514e36a5505d0e
zhouhuan <754865975@qq.com>
2021-11-19 15:41:44 +0800

    Updates

---
+++ precache-manifest.337e83893d9e9652a3a113970c46d478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb66e212d797dd7e7b84",
+    "url": "/DeFIL1.0/static/css/main.9cc6fdb7.chunk.css"
+  },
+  {
+    "revision": "cb66e212d797dd7e7b84",
+    "url": "/DeFIL1.0/static/js/main.cb66e212.chunk.js"
+  },
+  {
+    "revision": "f2aaf68ae5ee25500597",
+    "url": "/DeFIL1.0/static/css/1.575b2b56.chunk.css"
+  },
+  {
+    "revision": "f2aaf68ae5ee25500597",
+    "url": "/DeFIL1.0/static/js/1.f2aaf68a.chunk.js"
+  },
+  {
+    "revision": "06f2b4430d08e4df6ba5",
+    "url": "/DeFIL1.0/static/js/runtime~main.06f2b443.js"
+  },
+  {
+    "revision": "51909a9f85c43258f29b2f5294b6425c",
+    "url": "/DeFIL1.0/index.html"
+  }
+];
